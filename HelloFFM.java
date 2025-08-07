803058209388d3a0132943a2408d4f88600480bf
alina-yur <alina.yurenko@oracle.com>
2025-08-07 14:57:23 +0200

    finalize, add configurartion and readme

--- HelloFFM.java
+++ HelloFFM.java
@@ -1,16 +1,31 @@
+/*modified from https://dev.java/learn/ffm/native/ */
+
 import java.lang.foreign.*;
 import java.lang.invoke.MethodHandle;
+import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 
 public class HelloFFM {
     public static void main(String[] args) throws Throwable {
+        String pattern = (args.length > 0) ? args[0] : "********";
+
         Linker linker = Linker.nativeLinker();
-        MemorySegment strlen = linker.defaultLookup().find("strlen").orElseThrow();
-        MethodHandle handle = linker.downcallHandle(strlen, 
-            FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS));
-        
+        SymbolLookup stdLib = linker.defaultLookup();
+        MemorySegment strdup_addr = stdLib.find("strdup").get();
+
+        var layout = MemoryLayout.sequenceLayout(Long.MAX_VALUE, JAVA_BYTE);
+        FunctionDescriptor strdup_sig = FunctionDescriptor.of(
+                ValueLayout.ADDRESS.withTargetLayout(layout),
+                ValueLayout.ADDRESS.withTargetLayout(layout));
+
+        MethodHandle strdup_handle = linker.downcallHandle(strdup_addr, strdup_sig);
+
         Arena arena = Arena.ofShared();
-        MemorySegment str = arena.allocateFrom("Hello FFM!");
-        long length = (long) handle.invoke(str);
-        System.out.println(length);
+        MemorySegment nativeString = arena.allocateFrom(pattern);
+        MemorySegment duplicatedAddress = (MemorySegment) strdup_handle.invokeExact(nativeString);
+        String duplicated = duplicatedAddress.getString(0);
+
+        for (int i = 0; i < pattern.length(); i++) {
+            System.out.println(duplicated.substring(0, i + 1));
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# native-ffm
+# Native FFM
+
+This repository demonstrates the FFM API support in GraalVM Native Image and collecting required configuration in `reachability-metadata.json`.
+
+## Run:
+
+```shell
+java HelloFFM.java
+```
+
+## Collect the reachability configuration:
+
+```shell
+ java -agentlib:native-image-agent HelloFFM.java
+```
+
+## Build and run as Native Image:
+
+```shell
+javac HelloFFM.java
+native-image -H:+UnlockExperimentalVMOptions -H:+ForeignAPISupport -H:-UnlockExperimentalVMOptions HelloFFM
+./helloffm
+```
